0a5acb2dc3a427802f160062f0f1b82390cee400
Sir_Dave <lucasdavidd101@gmail.com>
2023-08-23 23:29:31 +0100

    made some chages to day 17

--- Day17_CharityFund.py
+++ Day17_CharityFund.py
@@ -9,9 +9,9 @@ class CharityFund:
         self.balance *= 1 + ReturnRate 
     def GetBalance(self):
         if self.balance < 0:
-            self.balance = -self.balance
-            print(f'You have entered a deficit of {self.balance}.')
-        return -self.balance
+            # self.balance = -self.balance
+            print(f'You have entered a deficit of {-self.balance}.')
+        return self.balance
     def IsDanger(self):
         if self.balance < 5000:
             print(f'Your fund is less than 50000. Be careful of your spendings.')
@@ -19,5 +19,9 @@ class CharityFund:
     
     
 HelpNeedy = CharityFund()
-HelpNeedy.SpendFund(100001)
+HelpNeedy.SpendFund(10001)
 print(f'Your balance is now {HelpNeedy.GetBalance()}.')
+HelpNeedy.SaveFund(20000)
+print(f'Your new balance is {HelpNeedy.GetBalance()}.')
+HelpNeedy.invest(0.90)
+print(f'Your new balance is {HelpNeedy.GetBalance()}.')
